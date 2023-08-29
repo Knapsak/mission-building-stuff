798568a6d015e3c1f843f72587a8f480f50b1a61
Knapsak <Knapsak@gmail.com>
2023-08-29 10:48:39 +0200

    Update Cyprus_Beacons.lua

--- Cyprus_Beacons.lua
+++ Cyprus_Beacons.lua
@@ -8,15 +8,28 @@
 BASE:TraceLevel(3)
 
 -- Create our UNITs on which we'll attach a BEACON
-local Aircraft = UNIT:FindByName("Unit1")
-local LandUnit = UNIT:FindByName("Unit2")
+local Aircraft = UNIT:FindByName("AFAC SA342 1")
+local Aircraft = UNIT:FindByName("AFAC SA342 2")
+local Aircraft = UNIT:FindByName("AFAC SA342 3")
+local Aircraft = UNIT:FindByName("AFAC SA342 4")
+local Aircraft = UNIT:FindByName("AFAC SA342 5") 
+local LandUnit = UNIT:FindByName("TACAN 10X")
+local LandUnit = UNIT:FindByName("TACAN 11X")
+local LandUnit = UNIT:FindByName("TACAN 12X")
 
 -- Now, let's start with the TACAN Beacon.
 -- Note that they are limited to Y band. Notice also that this particular TACAN can be homed on.
 local BeaconAircraft = Aircraft:GetBeacon()
-BeaconAircraft:AATACAN(6, "UNIT1", true)
+BeaconAircraft:AATACAN(5, "AFAC SA342 1", true)
+BeaconAircraft:AATACAN(6, "AFAC SA342 2", true)
+BeaconAircraft:AATACAN(7, "AFAC SA342 3", true)
+BeaconAircraft:AATACAN(8, "AFAC SA342 4", true)
+BeaconAircraft:AATACAN(9, "AFAC SA342 5", true)
+
 
 -- And let's setup the ground based radio beacon !
 -- Notice how this beacon will stop in 20 sec (last parameter).
 local BeaconLand = LandUnit:GetBeacon()
-BeaconLand:RadioBeacon("Morse.ogg", 129, radio.modulation.AM, 100, 20)
+BeaconLand:RadioBeacon("Morse.ogg", 129.1, radio.modulation.AM, 100, 20)
+BeaconLand:RadioBeacon("Morse.ogg", 129.2, radio.modulation.AM, 100, 20)
+BeaconLand:RadioBeacon("Morse.ogg", 129.3, radio.modulation.AM, 100, 20)
