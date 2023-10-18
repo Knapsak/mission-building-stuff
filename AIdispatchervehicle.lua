fd7ff9fb7f3a18892fdc2bbcfecd89a933ad3200
Knapsak <Knapsak@gmail.com>
2023-10-18 14:46:21 +0200

    Create AIdispatchervehicle.lua

---
+++ AIdispatchervehicle.lua
@@ -0,0 +1,7 @@
+An AI dispatcher object for a vehicle squadron, moving infantry from pickup zones to deploy zones.
+   local SetCargoInfantry = SET_CARGO:New():FilterTypes( "Infantry" ):FilterStart()
+   local SetAPC = SET_GROUP:New():FilterPrefixes( "APC" ):FilterStart()
+   local SetDeployZones = SET_ZONE:New():FilterPrefixes( "Deploy" ):FilterStart()
+
+   AICargoDispatcherAPC = AI_CARGO_DISPATCHER_APC:New( SetAPC, SetCargoInfantry, nil, SetDeployZones ) 
+   AICargoDispatcherAPC:Start()
