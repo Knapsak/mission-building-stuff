0a369d9b20d6432ba21339fb2acfe5ae4fbde353
Knapsak <Knapsak@gmail.com>
2023-10-30 10:42:44 +0200

    Update spawnvehicleatship.lua

--- spawnvehicleatship.lua
+++ spawnvehicleatship.lua
@@ -1,1 +1,10 @@
 SPAWN:SpawnFromUnit(HostUnit, MinHeight, MaxHeight, SpawnIndex)
+
+
+local SpawnStatic = STATIC:FindByName( StaticName )
+
+  -- Spawn from the static position at the height specified in the ME of the group template!
+  SpawnAirplanes:SpawnFromUnit( SpawnStatic )
+
+  -- Spawn from the static position at the height randomized between 2000 and 4000 meters.
+  SpawnAirplanes:SpawnFromUnit( SpawnStatic, 2000, 4000 )
