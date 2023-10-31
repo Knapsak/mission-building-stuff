7df32e8942044d6c7fcb9c1c683c06d69e0cea56
Knapsak <Knapsak@gmail.com>
2023-10-31 09:35:50 +0200

    Update CSAR mission.lua

--- CSAR mission.lua
+++ CSAR mission.lua
@@ -1,5 +1,24 @@
   -- Create downed "Pilot Wagner" in #ZONE "CSAR_Start_1" at a random point for the blue coalition
-   my_csar:SpawnCSARAtZone( "CSAR_Start_1", coalition.side.BLUE, "Pilot Wagner", true )
+   my_csar:SpawnCSARAtZone( ("CSAR_Start_1","CSAR_Start_2","CSAR_Start_3","CSAR_Start_4"), coalition.side.BLUE, "Ships Crew", true )
 
  --Create a casualty and CASEVAC request from a "Point" (VEC2) for the blue coalition --shagrat
  my_csar:SpawnCASEVAC(Point, coalition.side.BLUE)  
+
+  -- Create downed "Pilot Wagner" in #ZONE "CSAR_Start_2" at a random point for the blue coalition
+   my_csar:SpawnCSARAtZone( "CSAR_Start_2", coalition.side.BLUE, "Ships Crew", true )
+
+ --Create a casualty and CASEVAC request from a "Point" (VEC2) for the blue coalition --shagrat
+ my_csar:SpawnCASEVAC(Point, coalition.side.BLUE)  
+
+  -- Create downed "Pilot Wagner" in #ZONE "CSAR_Start_3" at a random point for the blue coalition
+   my_csar:SpawnCSARAtZone( "CSAR_Start_3", coalition.side.BLUE, "Ships Crew", true )
+
+ --Create a casualty and CASEVAC request from a "Point" (VEC2) for the blue coalition --shagrat
+ my_csar:SpawnCASEVAC(Point, coalition.side.BLUE)  
+
+  -- Create downed "Pilot Wagner" in #ZONE "CSAR_Start_4" at a random point for the blue coalition
+   my_csar:SpawnCSARAtZone( "CSAR_Start_4", coalition.side.BLUE, "Ships Crew", true )
+
+ --Create a casualty and CASEVAC request from a "Point" (VEC2) for the blue coalition --shagrat
+ my_csar:SpawnCASEVAC(Point, coalition.side.BLUE) 
+
